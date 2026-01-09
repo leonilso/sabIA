34fb8919e486235453403934f5a4a842f4752e9f
leonilso <wrublak.leonilso@escola.pr.gov.br>
2026-01-09 13:55:12 -0300

    modificando rota da api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL:'http://localhost:3000/api'
-  baseURL:'http://sabiaapi.leonilso.com.br/api'
+  baseURL:'http://sabia.leonilso.com.br/api'
 });
 
 // Adiciona o token em toda requisição

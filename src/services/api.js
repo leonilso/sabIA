1f249ac7d8157364094eac1cbf6d7325fa432cec
leonilso <wrublak.leonilso@escola.pr.gov.br>
2026-01-09 08:43:18 -0300

    Modificando rota paara server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL:'http://sabia.leonilso.com.br:3000/api'
+  // baseURL:'http://localhost:3000/api'
   baseURL:'http://sabiaapi.leonilso.com.br/api'
 });
 

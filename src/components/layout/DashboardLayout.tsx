05b2c85be85dafd889e5a782bc4d1ac67034f5f1
leonilso <wrublak.leonilso@escola.pr.gov.br>
2026-02-03 21:10:45 -0300

    bug fix: depois de 3 horas perdidas tentando resolver um erro no servidors descobri que meu roteador não quis aceitar uma ataque DDOS que meu fluxo de CI/CD fez na minha rede doméstica

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -14,7 +14,7 @@ export function DashboardLayout({ children, title, subtitle }: DashboardLayoutPr
       <AppHeader />
       <div className="flex">
         <AppSidebar />
-        <main className="flex-1 pt-6 px-6 bg-background rounded-tl-3xl">
+        <main className="flex-1 pt-6 pb-6 px-6 bg-background rounded-tl-3xl">
           <div className="bg-card rounded-3xl p-6 min-h-[calc(100vh-8rem)] shadow-card">
             {(title || subtitle) && (
               <div>
--- src/pages/Turmas.tsx
+++ src/pages/Turmas.tsx
@@ -122,7 +122,7 @@ export default function Turmas() {
 
       <Link
         to={`/turmas/criar`}
-        className="fixed bottom-12 right-3 h-12 flex bg-secondary hover:bg-secondary/90 text-secondary-foreground rounded-full px-10 py-2 shadow-lg gap-2"
+        className="fixed bottom-12 right-3 h-12 flex bg-secondary hover:bg-secondary/90 text-secondary-foreground rounded-full px-6 py-6 shadow-lg gap-2"
       >
         <Plus className="w-5 h-5" />
         Nova Turma
